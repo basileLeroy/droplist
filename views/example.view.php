2c4d67fcf90012afdf0af58626024802e7dd5173
basileLeroy <basile.leroy.jnm@hotmail.com>
2025-02-19 17:10:26 +0100

    Create Error & Example view pages

---
+++ views/example.view.php
@@ -0,0 +1,12 @@
+<?php require 'partials/header.php'; ?>
+<?php require 'partials/nav.php'; ?>
+
+<header>
+    <h1>Home</h1>
+</header>
+
+<main>
+    <p>This is the Home page</p>
+</main>
+
+<?php require 'partials/footer.php'; ?>
