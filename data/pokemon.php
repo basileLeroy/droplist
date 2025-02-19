c4ff573e59954b472f0680a6d6389fad5814fa98
basileLeroy <basile.leroy.jnm@hotmail.com>
2025-02-19 17:38:07 +0100

    Add prep for demo on objects

---
+++ data/pokemon.php
@@ -0,0 +1,23 @@
+<?php
+$pokemonList = [
+    ["name" => "Pikachu", "type" => "Electric", "rarity" => "Common", "image" => "https://img.pokemondb.net/artwork/large/pikachu.jpg"],
+    ["name" => "Charizard", "type" => "Fire/Flying", "rarity" => "Rare", "image" => "https://img.pokemondb.net/artwork/large/charizard.jpg"],
+    ["name" => "Bulbasaur", "type" => "Grass/Poison", "rarity" => "Common", "image" => "https://img.pokemondb.net/artwork/large/bulbasaur.jpg"],
+    ["name" => "Squirtle", "type" => "Water", "rarity" => "Common", "image" => "https://img.pokemondb.net/artwork/large/squirtle.jpg"],
+    ["name" => "Jigglypuff", "type" => "Normal/Fairy", "rarity" => "Uncommon", "image" => "https://img.pokemondb.net/artwork/large/jigglypuff.jpg"],
+    ["name" => "Gengar", "type" => "Ghost/Poison", "rarity" => "Rare", "image" => "https://img.pokemondb.net/artwork/large/gengar.jpg"],
+    ["name" => "Eevee", "type" => "Normal", "rarity" => "Common", "image" => "https://img.pokemondb.net/artwork/large/eevee.jpg"],
+    ["name" => "Snorlax", "type" => "Normal", "rarity" => "Uncommon", "image" => "https://img.pokemondb.net/artwork/large/snorlax.jpg"],
+    ["name" => "Dragonite", "type" => "Dragon/Flying", "rarity" => "Rare", "image" => "https://img.pokemondb.net/artwork/large/dragonite.jpg"],
+    ["name" => "Mewtwo", "type" => "Psychic", "rarity" => "Legendary", "image" => "https://img.pokemondb.net/artwork/large/mewtwo.jpg"],
+    ["name" => "Mew", "type" => "Psychic", "rarity" => "Mythical", "image" => "https://img.pokemondb.net/artwork/large/mew.jpg"],
+    ["name" => "Arcanine", "type" => "Fire", "rarity" => "Uncommon", "image" => "https://img.pokemondb.net/artwork/large/arcanine.jpg"],
+    ["name" => "Lucario", "type" => "Fighting/Steel", "rarity" => "Rare", "image" => "https://img.pokemondb.net/artwork/large/lucario.jpg"],
+    ["name" => "Gyarados", "type" => "Water/Flying", "rarity" => "Rare", "image" => "https://img.pokemondb.net/artwork/large/gyarados.jpg"],
+    ["name" => "Lapras", "type" => "Water/Ice", "rarity" => "Uncommon", "image" => "https://img.pokemondb.net/artwork/large/lapras.jpg"],
+    ["name" => "Alakazam", "type" => "Psychic", "rarity" => "Rare", "image" => "https://img.pokemondb.net/artwork/large/alakazam.jpg"],
+    ["name" => "Machamp", "type" => "Fighting", "rarity" => "Uncommon", "image" => "https://img.pokemondb.net/artwork/large/machamp.jpg"],
+    ["name" => "Garchomp", "type" => "Dragon/Ground", "rarity" => "Rare", "image" => "https://img.pokemondb.net/artwork/large/garchomp.jpg"],
+    ["name" => "Tyranitar", "type" => "Rock/Dark", "rarity" => "Rare", "image" => "https://img.pokemondb.net/artwork/large/tyranitar.jpg"],
+    ["name" => "Rayquaza", "type" => "Dragon/Flying", "rarity" => "Legendary", "image" => "https://img.pokemondb.net/artwork/large/rayquaza.jpg"]
+];
--- example.php
+++ example.php
@@ -1,3 +1,5 @@
 <?php
 
+// Let's learn about classes
+
 require_once "views/example.view.php";
