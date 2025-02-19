251f2d9442d46a7d791f17a5e1fb5a2737a2cc4f
basileLeroy <basile.leroy.jnm@hotmail.com>
2025-02-19 16:43:50 +0100

    Create View pages

---
+++ views/contact.view.php
@@ -0,0 +1,11 @@
+<?php require 'partials/header.php'; ?>
+<?php require 'partials/nav.php'; ?>
+<header>
+    <h1>Contact</h1>
+</header>
+
+<main>
+    <p>This is the contact page</p>
+</main>
+
+<?php require 'partials/footer.php'; ?>
