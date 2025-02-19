93e6f36428b26759b434613823df01a1e0169c99
basileLeroy <basile.leroy.jnm@hotmail.com>
2025-02-19 17:52:24 +0100

    Add example card + styling

--- views/example.view.php
+++ views/example.view.php
@@ -6,7 +6,16 @@
 </header>
 
 <main>
-    <p>This is the Example page</p>
+    <h2>This is the Example page</h2>
+
+    <div class="gallery">
+        <!-- <div class="card">
+            <img src="https://img.pokemondb.net/artwork/large/pikachu.jpg" alt="Pikachu">
+            <h3>Pikachu</h3>
+            <p>Type: Electric</p>
+            <p>Rarity: <strong>Common</strong></p>
+        </div> -->
+    </div>
 </main>
 
 <?php require 'partials/footer.php'; ?>
